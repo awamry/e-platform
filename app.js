106ab82b77ef4924c3214e81596489968d4e2e63
Awamry <omarelawamry@gmail.com>
2019-05-05 02:03:57 +0200

    Addded env variable in app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-/* const db = require('./database/config/database')
+const db = require('./database/config/database')
 const Users = require('./database/models/Users')
 const Questions = require('./database/models/Questions')
-const Games = require('./database/models/Games') */
+const Games = require('./database/models/Games')
 
 const bodyParser = require('body-parser')
 const app = express()
@@ -18,28 +18,29 @@ app.use((req, res, next) => {
     statusCode: '404'
   })
 })
+if (process.env.DATABASE_URL) {
+  ;(async () => {
+    await db.sync({ force: true })
 
-app.listen(3000, async () => {
-  /* await db.sync({ force: true })
+    await Users.create({
+      name: 'Omar',
+      email: 'omarelawamry@gmail.com',
+      password: 'awamry'
+    })
 
-  await Users.create({
-    name: 'Omar',
-    email: 'omarelawamry@gmail.com',
-    password: 'awamry'
-  })
+    await Games.create({
+      name: 'Name',
+      type: 'True or false',
+      userId: 1
+    })
+    await Questions.create({
+      question: 'My name is omar',
+      answer: 'true',
+      gameId: 1
+    })
+  })()
+}
 
-  await Games.create({
-    name: 'Name',
-    type: 'True or false',
-    userId: 1
-  })
-  await Questions.create({
-    question: 'My name is omar',
-    answer: 'true',
-    gameId: 1
-  }) */
-
-  console.log(`listening on port 3000`)
-})
+app.listen(3000, async () => console.log('App is running on port 3000'))
 
 module.exports = app
